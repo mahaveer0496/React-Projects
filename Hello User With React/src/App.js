bb5e5382b8768c184d668a390f071bb67a1d5a61
Mahaveer sharma <Mahaveer sharma>
2017-03-16 23:57:40 +0530

    ES5 to ES6 conversion

--- Hello User With React/src/App.js
+++ Hello User With React/src/App.js
@@ -2,79 +2,79 @@ import * as React from 'react';
 import './App.css';
 
 // GREETING COMPONENT----------------------------
-var MyGreeting = React.createClass({
-  render: function () {
-    return(
-      <div>
-        <h1 className="text-center">Hi {this.props.name}</h1>
-        <p className="lead text-center">{this.props.message}</p> 
-      </div>
-    )
-  }
-})
+function MyGreeting(props) {
+  return (
+    <div>
+      <h1 className="text-center">Hi {props.name}</h1>
+      <p className="lead text-center">{props.message}</p>
+    </div>
+  )
+}
 
 // FORM COMPONENT ---------------------------
-var MyForm = React.createClass({
-  changeName: function (e) {
-      e.preventDefault();      
-      var name = this.refs.name.value;
-      var message = this.refs.message.value;
-      console.log(`${name} -- ${message}`); 
-      if(name.length === 0 || message.lenght === 0){
-        name = "pls enter your name";
-        message = "pls enter your message"
-      }
-      var obj = {
-        name: name,
-        message: message
-      }
-      this.props.onState(obj)
-      this.refs.name.value = '';
-      this.refs.message.value = '';      
-  },
-  render:function () { 
-    return(
+class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.changeName = this.changeName.bind(this)
+  }
+  changeName(e) {
+    e.preventDefault();
+    var name = this.refs.name.value;
+    var message = this.refs.message.value;
+    console.log(`${name} -- ${message}`);
+    if (name.length === 0 || message.lenght === 0) {
+      name = "pls enter your name";
+      message = "pls enter your message"
+    }
+    var obj = {
+      name: name,
+      message: message
+    }
+    this.props.onState(obj)
+    this.refs.name.value = '';
+    this.refs.message.value = '';
+  }
+  render() {
+    return (
       <form onSubmit={this.changeName}>
-        <input type="text" placeholder="enter your name" className="form-control" ref="name" required/>
-        <br/>
-        <input type="text" placeholder="enter your message" className="form-control" ref="message" required/>
-        <br/>
-        <input type="submit" className="btn btn-primary" value="submit"/>
+        <input type="text" placeholder="enter your name" className="form-control" ref="name" required />
+        <br />
+        <input type="text" placeholder="enter your message" className="form-control" ref="message" required />
+        <br />
+        <input type="submit" className="btn btn-primary" value="submit" />
       </form>
     )
-   }
-})
+  }
+}
 
-// APP COMPONENT-------------------------------
-var App = React.createClass({
-  getDefaultProps: function () {
-    return {
-      name: "Mahaveer",
-      message: "this is a default message"
-    }
-   },
-   getInitialState: function () {
-     return {
-       name: this.props.name,
-       message: this.props.message       
-     }
-   },
-   stateSetter: function (obj) {  
-     this.setState({
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.props = ({
+      name: "mahaveer",
+      message: "a default message"
+    })
+    this.state = ({
+      name: this.props.name,
+      message: this.props.message
+    })
+    this.stateSetter = this.stateSetter.bind(this)
+  }
+  stateSetter(obj) {
+    this.setState({
       name: obj.name,
       message: obj.message
-     })
-   },
-  render: function () {
-    return(
+    })
+  }
+  render() {
+    return (
       <div className="container">
         <div className="jumbotron">
-          <MyGreeting name={this.state.name} message={this.state.message}/>
-          <MyForm onState={this.stateSetter}/>
+          <MyGreeting name={this.state.name} message={this.state.message} />
+          <MyForm onState={this.stateSetter} />
         </div>
       </div>
     )
   }
-})
-
+}
 export default App;
